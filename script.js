062b9e56039d98d413b04274b84420bab62f300b
estefhanecruz <estefhanecruz@gmail.com>
2025-07-14 11:47:23 -0300

    Inclusão de Índices e Substrings

--- script.js
+++ script.js
@@ -16,6 +16,20 @@ console.log(tituloSemLetraA);
 tituloSemLetraA = titulo.replaceAll('a', 'x') //Troca TODAS as ocorrência do caractere a
 console.log(tituloSemLetraA);
 
+//Índices
+let index = titulo.indexOf('javascript'); //Mostra a posição da primeira ocorrência de um determinado valor em uma string ou array
+console.log(index);
+
+//Substrings
+let subtitulo = titulo.substring(12, 16) //Extrai uma parte de uma string, retornando uma nova string que contém os caracteres especificados entre dois índices
+console.log(subtitulo);
+
+subtitulo = titulo.substring(index) // Faz a extração a partir da posição index que já foi calculada
+console.log(subtitulo);
+
+subtitulo = titulo.slice(-index); //Permite que se coloque índices negativos
+console.log(subtitulo);
+
 
 
 
